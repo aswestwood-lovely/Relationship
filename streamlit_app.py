d669bc3e6346abc4630d3a0309c64287f06c62e9
Lovely1 Solutions <aswestwood@gmail.com>
2026-01-20 23:01:06 -0800

    Update streamlit_app.py
    added line spacing

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.title("🎈 Lovely1 Solutions")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).\n\n"
 
-    "Click on the application below that you are testing."
+    " Click on the application below that you are testing.\n\n"
 
-"Be very detailed in documenting errors and suggested updates. All feedback is appreciated."
+" Be very detailed in documenting errors and suggested updates. All feedback is appreciated."
 )
